2ddf0427fcfa7518d3e5226ab8bd4e2d3330a120
Ertuğrul EŞOL <ertugrul.esol@gmail.com>
2021-02-04 00:03:07 +0300

    3. Şubat Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,9 +1,12 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //O -> Open Closed Principle
     class Program
     {
         static void Main(string[] args)
@@ -11,8 +14,8 @@ namespace ConsoleUI
             //Abstract -> Soyutlar (Interfaces, abstracts...) (Referans tutuculardır.)
             //Concrete -> Somutlar (İşi yapan sınıflar)
 
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            foreach (var product in productManager.GetAll())
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice(40,100))
             {
                 Console.WriteLine(product.ProductName);
             }
